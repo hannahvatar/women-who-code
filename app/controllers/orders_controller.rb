4c0661adeacd13df53952aef7be6a053c3657b8c
Anabelle Dubois <anabelle.dubois@icloud.com>
2024-12-17 14:09:38 -0500

    Main workflow without payment method added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,32 @@
+# app/controllers/orders_controller.rb
 class OrdersController < ApplicationController
   def new
     @order = Order.new
     @variant = ProductVariant.find(params[:variant_id])
+    @total_amount = @variant.price * (params[:quantity] || 1).to_i
   end
 
   def create
     @order = Order.new(order_params)
+    @variant = ProductVariant.find(params[:variant_id])
+    quantity = params[:quantity].to_i || 1
+
+    # Calculate total amount
+    @order.total_amount = @variant.price * quantity
 
     if @order.save
       # Create the order item
       OrderItem.create!(
         order: @order,
         product_variant_id: params[:variant_id],
-        quantity: params[:quantity],
-        unit_price: ProductVariant.find(params[:variant_id]).price
+        quantity: quantity,
+        unit_price: @variant.price
       )
 
       redirect_to @order, notice: 'Order was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  def show
-    @order = Order.includes(order_items: { product_variant: [:product, :color, :size] }).find(params[:id])
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 # Clear existing data
 puts "Cleaning database..."
+# Must destroy in correct order due to foreign key constraints
+OrderItem.destroy_all
+Order.destroy_all
+ProductVariant.destroy_all
 Product.destroy_all
 Color.destroy_all
 Size.destroy_all
@@ -15,14 +19,18 @@ product = Product.create!(
 # Create colors
 puts "Creating colors..."
 blue = Color.create!(name: "Blue")
-red = Color.create!(name: "Red")
-black = Color.create!(name: "Black")
+purple = Color.create!(name: "Purple")
+coral = Color.create!(name: "Coral")
+crimson = Color.create!(name: "Crimson")
 
 # Create sizes
 puts "Creating sizes..."
 small = Size.create!(name: "S")
 medium = Size.create!(name: "M")
 large = Size.create!(name: "L")
+xlarge = Size.create!(name: "XL")
+xxlarge = Size.create!(name: "2XL")
+xxxlarge = Size.create!(name: "3XL")
 
 # Create product variants with images
 puts "Creating product variants..."
@@ -33,7 +41,7 @@ ProductVariant.create!(
   size: small,
   price: 29.99,
   stock: 10,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/blue_tshirt_front_odinvj.jpg"
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_blue_anl1zo.jpg"
 )
 
 ProductVariant.create!(
@@ -42,7 +50,7 @@ ProductVariant.create!(
   size: medium,
   price: 29.99,
   stock: 15,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/blue_tshirt_front_odinvj.jpg"
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_blue_anl1zo.jpg"
 )
 
 ProductVariant.create!(
@@ -51,63 +59,199 @@ ProductVariant.create!(
   size: large,
   price: 29.99,
   stock: 20,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/blue_tshirt_front_odinvj.jpg"
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_blue_anl1zo.jpg"
 )
 
-# Red variants
 ProductVariant.create!(
   product: product,
-  color: red,
+  color: blue,
+  size: xlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_blue_anl1zo.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: blue,
+  size: xxlarge,
+  price: 29.99,
+  stock: 15,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_blue_anl1zo.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: blue,
+  size: xxxlarge,
+  price: 29.99,
+  stock: 20,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_blue_anl1zo.jpg"
+)
+
+# Purple variants
+ProductVariant.create!(
+  product: product,
+  color: purple,
   size: small,
   price: 29.99,
   stock: 10,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/red_tshirt_front_kpxz0n.jpg"
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458160/sweater_purple_zixn0s.jpg"
 )
 
 ProductVariant.create!(
   product: product,
-  color: red,
+  color: purple,
   size: medium,
   price: 29.99,
-  stock: 15,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/red_tshirt_front_kpxz0n.jpg"
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458160/sweater_purple_zixn0s.jpg"
 )
 
 ProductVariant.create!(
   product: product,
-  color: red,
+  color: purple,
   size: large,
   price: 29.99,
-  stock: 20,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/red_tshirt_front_kpxz0n.jpg"
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458160/sweater_purple_zixn0s.jpg"
 )
 
-# Black variants
 ProductVariant.create!(
   product: product,
-  color: black,
+  color: purple,
+  size: xlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458160/sweater_purple_zixn0s.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: purple,
+  size: xxlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458160/sweater_purple_zixn0s.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: purple,
+  size: xxxlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458160/sweater_purple_zixn0s.jpg"
+)
+
+# Coral variants
+ProductVariant.create!(
+  product: product,
+  color: coral,
   size: small,
   price: 29.99,
   stock: 10,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/black_tshirt_front_gvhpgv.jpg"
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_coral_dsso00.jpg"
 )
 
 ProductVariant.create!(
   product: product,
-  color: black,
+  color: coral,
   size: medium,
   price: 29.99,
-  stock: 15,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/black_tshirt_front_gvhpgv.jpg"
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_coral_dsso00.jpg"
 )
 
 ProductVariant.create!(
   product: product,
-  color: black,
+  color: coral,
   size: large,
   price: 29.99,
-  stock: 20,
-  image_url: "https://res.cloudinary.com/dp5zxwvrx/image/upload/v1701910901/black_tshirt_front_gvhpgv.jpg"
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_coral_dsso00.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: coral,
+  size: xlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_coral_dsso00.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: coral,
+  size: xxlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_coral_dsso00.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: coral,
+  size: xxxlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_coral_dsso00.jpg"
+)
+
+# Crimson variants
+ProductVariant.create!(
+  product: product,
+  color: crimson,
+  size: small,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_crimson_kplpcr.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: crimson,
+  size: medium,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_crimson_kplpcr.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: crimson,
+  size: large,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_crimson_kplpcr.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: crimson,
+  size: xlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_crimson_kplpcr.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: crimson,
+  size: xxlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_crimson_kplpcr.jpg"
+)
+
+ProductVariant.create!(
+  product: product,
+  color: crimson,
+  size: xxxlarge,
+  price: 29.99,
+  stock: 10,
+  image_url: "https://res.cloudinary.com/diwuyv3c8/image/upload/v1734458159/sweater_crimson_kplpcr.jpg"
 )
 
 puts "Finished!"
