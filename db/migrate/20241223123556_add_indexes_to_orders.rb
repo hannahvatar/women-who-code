22b570b7341d6c23118cb3633b2ff3a40cfcce5d
Anabelle Dubois <anabelle.dubois@icloud.com>
2024-12-26 10:02:46 -0500

    Fixing migration issue

--- db/migrate/20241223123556_add_indexes_to_orders.rb
+++ db/migrate/20241223123556_add_indexes_to_orders.rb
@@ -1,6 +1,6 @@
 class AddIndexesToOrders < ActiveRecord::Migration[7.1]
   def change
-    add_index :orders, :checkout_session_id
-    add_index :orders, :payment_intent_id
+    add_index :orders, :checkout_session_id unless index_exists?(:orders, :checkout_session_id)
+    add_index :orders, :payment_intent_id unless index_exists?(:orders, :payment_intent_id)
   end
 end
